7e16e0df1ad982966f882adb50c8525af5024e2d
SmsS4 <smss.lite@gmail.com>
2022-06-25 14:33:10 +0430

    Add logic

---
+++ discord_status_changer/config.py
@@ -0,0 +1,52 @@
+# discord token
+# use https://pcstrike.com/how-to-get-discord-token/ to get your discord token
+TOKEN = "NTk0MDc4NzQ1NjE5NTk1MjY0.GrkCX_.OtzID81jcnt6Jnpl6L49A72dBSBeDHt7Tody7M"
+
+# dict from process name to tuple of status and emoji
+PROCESS = {
+    'pycharm.sh': ("I'm working pydudes", "👨‍💻"),
+    'metaeditor64.ex': ("I'm working dudes. MQL SUCKS!", "🤑"),
+}
+
+# daily quotes list
+QUOTES = [
+    '“We Make The World What It Is, By What We Do. All Of Us.” – Iron Mike',
+    '“So Get Up. Dust Yourself Off. Try Again.” – Deacon',
+    '“Do You Know Why We Keep Going? Because What The Hell Else Are We Gonna Do?” – Deacon',
+    'There is nothing permanent, except change.',
+    "Careful, he's American. The metric system makes him angry.",
+    "When you deal with the best, you die like the rest!",
+    "If Mama ain't happy, nobody happy! So you'd better make Mama happy!",
+    "I don't know how I'm gonna fix this, but I'm sure as hell gonna try. - Kyle Crane",
+    "“Dreams have a nasty habit of going bad when you’re not looking.” – Max Payne",
+    "“Did I ever tell you the definition of insanity?” – Far Cry 3",
+    "“Waka Waka Waka” – Pacman",
+    "“The cake is a lie” – Portal",
+    "RUOK?",
+]
+
+# maximum number of allowed error
+MAX_ERROR = 10
+
+
+
+
+URL = "https://discord.com/api/v9/users/@me/settings"
+
+HEADERS = {
+    'accept': "*/*",
+    'accept-encoding': "gzip, deflate, br",
+    'accept-language': "en-US,en;q=0.9",
+    'authorization': TOKEN,
+    'content-type': "application/json",
+    'origin': "https://discord.com",
+    'referer': "https://discord.com/channels/@me",
+    'sec-ch-ua': "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
+    'sec-ch-ua-mobile': "?0",
+    'sec-ch-ua-platform': "\"Linux\"",
+    'sec-fetch-dest': "empty",
+    'sec-fetch-mode': "cors",
+    'sec-fetch-site': "same-origin",
+    'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+    'cache-control': "no-cache",
+}
---
+++ discord_status_changer/status_changer.py
@@ -0,0 +1,84 @@
+import datetime
+import time
+import traceback
+from typing import Optional, Tuple
+
+import psutil
+import requests
+
+from discord_status_changer import config
+from discord_status_changer.logger import get_logger
+
+logger = get_logger('status', split=' ')
+
+
+def get_process_quote() -> Optional[str]:
+    """
+    If any process of `config.PROCESS` is running returns
+    specified quote otherwise returns None
+    """
+    for proc in psutil.process_iter():
+        try:
+            name = proc.name()
+            if name in config.PROCESS:
+                return config.PROCESS[name]
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return None
+
+
+def get_daily_quote() -> Tuple[str, str]:
+    """
+    Gets quote of the day
+    """
+    days = (datetime.datetime.now().date() - datetime.datetime(1970, 1, 1).date()).days
+    return config.QUOTES[days % len(config.QUOTES)]
+
+
+def get_quote() -> dict:
+    """
+    Gets quote for showing in discord
+    """
+    q = get_process_quote()
+    if q is None:
+        q = get_daily_quote()
+        text = q
+        emoji = None
+    else:
+        text = q[0]
+        emoji = q[1]
+    return {
+        "custom_status":
+            {
+                "text": text,
+                "emoji_name": emoji
+            }
+    }
+
+
+def main():
+    logger.info("Start status changer")
+    last_payload = None
+    error_cnt = 0
+    while error_cnt < config.MAX_ERROR:
+        payload = get_quote()
+        if str(last_payload) != str(payload):
+            logger.info('New payload %s', payload)
+            try:
+                response = requests.request("PATCH", config.URL, json=payload, headers=config.HEADERS)
+                last_payload = payload
+                logger.info("Status code %d", response.status_code)
+            except ConnectionError as e:
+                logger.error("Connection error")
+                time.sleep(60)
+            except Exception as e:
+                error_cnt += 1
+                logger.error("Failed to change status %s\n%s", e, traceback.format_exc())
+                time.sleep(60)
+        time.sleep(60)
+
+    logger.error("MAX_ERROR exceeded")
+
+
+if __name__ == '__main__':
+    main()
